7c946c055dafa465887500d1c7869093310f7db6
Niken-Amalia <nknaml05@gmail.com>
2022-03-10 20:12:55 +0700

    menambahkan file Variable.php

---
+++ Variable.php
@@ -0,0 +1,13 @@
+<?php
+    $num1 = 4; //membuat variabel pertama
+    $num2 = 2; //membuat variabel kedua
+    $num3 = 3; //membuat variabel ketiga
+
+    $hasil1=$num1 +$num2 +$num3; //menambah variabel baru untuk melihat hasil dari operasi 3 variabel pertama 
+    $hasil2=$num1 -$num2 -$num3;
+    $hasil3=$num1 *$num2 *$num3;
+
+    echo"Hasil Operasi Matematika yang Pertama adalah $hasil1 <br>"; //menampilkan hasil operasi matematika ke layar
+    echo"Hasil Operasi Matematika yang Kedua adalah $hasil2 <br>";
+    echo"Hasil Operasi Matematika yang Ketiga adalah $hasil3 <br>";
+?>
